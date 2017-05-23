1fcadd69858e70e5bb4afd91165ad386fb913396
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-23 15:15:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  end
+  if array.length == 2
+    return array.join(" and ")
+  end
 
+add_commas = array.take(array.length - 1)
+oxford_string = add_commas.join(", ")
+# binding.pry
+oxford_string <<", and "
+oxford_string << array.last
+oxford_string
 end
